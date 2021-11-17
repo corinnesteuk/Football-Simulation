dc1eedf8ce2b545e0e127f3ddd95299cb7a7c984
corinne steuk <corinnesteuk@corinnes-MacBook-Pro.local>
2021-11-17 15:50:46 -0600

    november 17

--- Binomial Simulation.R
+++ Binomial Simulation.R
@@ -1,42 +1,49 @@
+library(Rlab)
 #FIRST DOWN CHECK
 #After each play, we need to increment the down they are on, check to see if they have reached a first down, 
 #and see if they have surpassed 4th down without completing 10 yards
-function4 <- function()
+touchdown_check <- function(tot_yrd){
   if (tot_yrd >= 75){
     return(TRUE)
-  }else if(d_yrd >= 10){
-    d_cnt <-1
+  }}
+firstdown_check <- function(d_yrd, d_cnt){
+  if(d_yrd >= 10){
+    d_cnt <- 1
+    d_yrd <- 0
+    down_info <- c(d_cnt, d_yrd)
+    return(down_info)
+  }else{
+    down_info <- c(d_cnt, d_yrd)
+    return(down_info)
   }
-  if (d_cnt ==4){
+}
+fourthdown_check <- function(d_cnt){
+  if (d_cnt > 4){
     return(FALSE)
+  }
 }
 
+d_cnt <- firstdown_check(d_yrd, d_cnt)[1]
+d_yrd <- firstdown_check(d_yrd, d_cnt)[2]
 #RUN PLAY
-function2 <- function()
+run_play <- function(){
   #randomly sample from normal distribution to return yardage of run
-  yardage <- rnorm(1, mean = 3, sd = )
-  i = 0
-  n = 10
-  while (i<=n){
-  if ((yardage > 3) || (yardage < 75)){
-    break
-  }else{i = i+1}
-  }
-  if(complete_check ==1){
-    d_yrd <- d_yrd + yardage
-    tot_yrd <- tot_yrd + yardage
-    tot_down <- tot_down + 1
-    d_cnt <- d_cnt +1
-  }else{
-    tot_down <- tot_down + 1
-    d_cnt <- d_cnt +1
-  }
-  #Call function4: First Down Check
-  #Call function5: Touchdown Check
+  yardage <- round(rnorm(1, mean = 3, sd = 3))
+  print(yardage)
+  d_yrd <- d_yrd + yardage
+  tot_yrd <- tot_yrd + yardage
+  tot_down <- tot_down + 1
+  d_cnt <- d_cnt +1
+  
+  touchdown_check(tot_yrd)
+  d_cnt <- firstdown_check(d_yrd, d_cnt)[1]
+  d_yrd <- firstdown_check(d_yrd, d_cnt)[2]
+  fourthdown_check(d_cnt)
+}
    ##END OF FUNCTION2
   
   ##PASS PLAY
-function3 <- function(){
+pass_play <- function(){
   #Randomly sample numbers from log distribution for yardage
   i = 0
   n = 10
@@ -56,7 +63,8 @@ function3 <- function(){
     return(p_comp)
   }
   p_comp <- p_comp_fn(yardage)
-  complete_check <- rbern(1,prob = p_comp)
+  complete_check <- rbern(1, prob = p_comp)
+  complete_check
   if(complete_check ==1){
    d_yrd <- d_yrd + yardage
    tot_yrd <- tot_yrd + yardage
@@ -66,39 +74,40 @@ function3 <- function(){
     tot_down <- tot_down + 1
     d_cnt <- d_cnt +1
   }
-  #Call function4: First Down Check
-  #Call function5: Touchdown Check
- }##END OF FUNCTION3
+  touchdown_check(tot_yrd)
+  d_cnt <- firstdown_check(d_yrd, d_cnt)[1]
+  d_yrd <- firstdown_check(d_yrd, d_cnt)[2]
+  fourthdown_check(d_cnt)
+}
+ ##END OF FUNCTION3
 
 
 
 ##Beginning of Play Simulation
 # Instantiating Downs & Counts
 d_yrd <- 0
-t_yrd <- 0
-t_down <- 1
+tot_yrd <- 0
+tot_down <- 0
 d_cnt <- 1
 
 #Create loop of n simulations
 #Choose either pass or run play
-function1 <- function(){
-  binom.sim <- function(n, p){
-    X <- 0 
-    px <- (1-p)^n
-    Fx <- px
-    U <- runif(1)
-    while (Fx < U){
-      X <- X +1
-      px <- px*p/(1-p)*(n-X+1)/X
-      Fx <- Fx +px 
-    }
-    return(X)
-  }
- if (binom.sim(1, .567) ==1){
-   #Go to Function2 --> Run Play
+nsim <- 10000
+j <- 0
+while (j<= nsim){
+  decision <- rbern(1, .567)
+ if (decision ==1L){
+   play_result <- run_play()
    
  }else{
-   #Go to Function3 --> Pass Play
+   play_result <- pass_play()
  }
-
+ # if (play_result == TRUE){
+ #   break
+ # }else if (play_result ==FALSE){
+ #   break
+ #}else{j =j+1}
+  j = j+1
 }
+  
+
