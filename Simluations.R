50f6d1d6756bf035db1e30037d3bf0278e38d83a
corinne steuk <corinnesteuk@corinnes-MacBook-Pro.local>
2021-11-30 09:50:34 -0600

    Nov30

--- Simluations.R
+++ Simluations.R
@@ -1,19 +1,15 @@
-<<<<<<< HEAD
+
 library(ggplot2)
-=======
+
 # 
->>>>>>> 42606495db929a8ff2c65f3c1f021e52b015f0f4
-# 
+
 # ##Run Mean = 3
 # run mean = 3
 # pass mean = log(10)
 # p_comp base = .97
 # prob_td = 0.3546
-<<<<<<< HEAD
 # avrg_plays = 4.3492 
-=======
 # avrg_plays = 4.3492
->>>>>>> 42606495db929a8ff2c65f3c1f021e52b015f0f4
 # 
 # run mean = 3
 # pass mean = log(10)
@@ -180,7 +176,6 @@ emp.data <- data.frame(
   sim_id = c (1:27),
   run_mean = c(3, 3, 3, 3, 3, 3, 3, 3, 3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 4, 4, 4, 4, 4, 4, 4, 4, 4),
   pass_mean = c(replicate(3, c('log(10)', 'log(10)', 'log(10)', 'log(5)', 'log(5)', 'log(5)', 'log(15)', 'log(15)', 'log(15)'))),
-<<<<<<< HEAD
   p_comp_base = rep(c(rep(c(.97, .9, .95), 3)), 3),
   prob_td = c(0.3546, 0.0401, 0.18459, 0.3272, 0.0537, 0.2019, 0.364, 0.0319, 0.1729, 0.2063, 0.012, 0.0896, 0.1665, 0.0126, 0.0793, 0.2248, 0.0073, 0.0884, 0.5421, 0.1233, 0.3485, 0.5568, 0.1603, 0.3862, 0.5201, 0.0908, 0.3161),
   avrg_plays = c(4.3492, 0.6493, 2.5527, 4.6803, 0.957, 3.1313, 4.175, 0.4989, 2.1708, 2.3883, 0.1956, 1.1444, 2.3441, 0.2194, 1.1926, 2.3141, 0.1038, 1.0319, 6.6756, 0.1233, 4.8535, 7.8982, 2.733, 5.8579, 5.8742, 1.4446, 4.0751))
@@ -195,17 +190,16 @@ emp.data
 ggplot(data = emp.data, aes(x = run_mean, y = prob_td, colour = pass_mean)) + geom_point(aes(shape = factor(p_comp_base)))+ggtitle("Touchdown Probability Simulations") + 
   ylab("Touchown Probability") +
   xlab("Run Mean") +
-  scale_fill_discrete(name = "Pass Mean")
+  scale_shape_discrete(name = "Pass Completion Base")+
+  scale_color_discrete(name = "Pass Mean")
 
 
 ggplot(data = emp.data, aes(x = run_mean, y = avrg_plays, colour = pass_mean)) + 
   geom_point(aes(shape = factor(p_comp_base)))+ggtitle("Average Play Count (Given TD) Simulations") + 
   ylab("Average Play Count")+
-  xlab("Run Mean") 
+  xlab("Run Mean") +
+  scale_shape_discrete(name = "Pass Completion Base")+
+  scale_color_discrete(name = "Pass Mean")
 
                                                                      
-=======
-  p_comp_base = rep(c(rep(c(.97, .9, .95), 3)), 3))
 
-emp.data
->>>>>>> 42606495db929a8ff2c65f3c1f021e52b015f0f4
